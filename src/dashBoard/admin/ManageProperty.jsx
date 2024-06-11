c890fed0735e9a66ddc86e60e3352a23995026d8
shihabosm <shihabshamim767@gmail.com>
2024-06-12 05:01:35 +0600

    manage property by admin

--- src/dashBoard/admin/ManageProperty.jsx
+++ src/dashBoard/admin/ManageProperty.jsx
@@ -1,9 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 
 const ManageProperty = () => {
+    const axiosSecure =useAxiosSecure()
+    const {data:manageProperty}=useQuery({
+        queryKey:['property'],
+        queryFn:async () =>{
+            try{
+                const {data} =await  axiosSecure('/property')
+                return data
+
+            }
+            catch{
+                console.log(error)
+            }
+        },
+        initialData:[]
+    })
     return (
         <div>
-            <h2>This is manage property </h2>
+            <h2 className="text-center text-xl font-bold bg-yellow-300 p-4">Manage Property {manageProperty.length} </h2>
+            <div>
+                
+            </div>
             
         </div>
     );
--- src/dashBoard/agent/Update.jsx
+++ src/dashBoard/agent/Update.jsx
@@ -1,9 +1,29 @@
 import { useParams } from "react-router-dom";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 
 const Update = () => {
-    const params = useParams()
-    console.log(params.id)
+    const axiosSecure =useAxiosSecure()
+    const params= useParams()
+    // console.log(id)
+    const {data}=useQuery({
+        queryKey:[`/property/${params.id}`],
+        queryFn : async () =>{
+           try{
+            const res =await axiosSecure.get(`/property/${params.id}`)
+            return res.data
+           }
+           catch{
+            console.log(error)
+           }
+        }
+    })
+    console.log(data)
+
+    
+    
     return (
         <div> <h2>updated</h2>
             
