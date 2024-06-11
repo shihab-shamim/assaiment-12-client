fb908456e9e00cb065b1121d951d39c876cca3cf
shihabosm <shihabshamim767@gmail.com>
2024-06-12 05:43:03 +0600

    Your commit message here

--- src/dashBoard/admin/ManageProperty.jsx
+++ src/dashBoard/admin/ManageProperty.jsx
@@ -21,11 +21,47 @@ const ManageProperty = () => {
     return (
         <div>
             <h2 className="text-center text-xl font-bold bg-yellow-300 p-4">Manage Property {manageProperty.length} </h2>
-            <div>
-                
-            </div>
+           
+          
+           <div className="overflow-x-auto">
+  <table className="table table-zebra  ">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>Number</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Title</th>
+        <th>Location</th>
+        <th>price</th>
+        <th>Verify</th>
+        <th>Reject</th>
+
+      </tr>
+    </thead>
+    <tbody>
+   {
+    manageProperty.map((property,i)=><tr key={property._id}>
+        <th>{i+1}</th>
+        <td>{property.agentName}</td>
+        <td>{property.agentEmail}</td>
+        <td>{property.title}</td>
+        <td>{property.location}</td>
+        <td>{property.minPrice}  - {property.maxPrice} $</td>
+        <td><button className="btn btn-ghost btn-sm bg-green-600">verify</button></td>
+        <th><button className="btn btn-ghost btn-sm bg-red-500">Reject</button></th>
+
+      </tr>)
+   }
+      
+      
+    </tbody>
+  </table>
+</div>
+          
+           </div>
             
-        </div>
+      
     );
 };
 
