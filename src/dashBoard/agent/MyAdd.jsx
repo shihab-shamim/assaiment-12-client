d1b3e3b9f6a3e61a1adf76ec901e121100364a58
shihabosm <shihabshamim767@gmail.com>
2024-06-12 03:09:31 +0600

    delete and updated

--- src/dashBoard/agent/MyAdd.jsx
+++ src/dashBoard/agent/MyAdd.jsx
@@ -1,19 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useAuth from "../../hooks/useAuth";
+import AddCard from "./AddCard";
 
 
 const MyAdd = () => {
+    const {user}=useAuth()
     const axiosSecure =useAxiosSecure()
-    const {data}=useQuery({
+    const {data:properties,refetch,isPending}=useQuery({
         queryKey:['property'],
         queryFn:async () => {
-            const res = await axiosSecure.get()
+            const res = await axiosSecure.get(`/property/${user?.email}`)
+            return res.data
 
-        }
+        },
+        initialData:[]
     })
+    
+   
     return (
         <div>
-            <h2 className="text-center text-xl font-bold bg-blue-500 text-white p-4"> My added properties section</h2>
+            <h2 className="text-center text-xl font-bold bg-blue-500 text-white p-4"> My added properties section {properties.length}</h2>
+
+            <div className="grid grid-cols-1  lg:grid-cols-2 xl:grid-cols-3">
+                {
+                    properties.map(property => <AddCard refetch={refetch}  property={property} user={user}  key={property._id}></AddCard>)
+                }
+            </div>
             
         </div>
     );
